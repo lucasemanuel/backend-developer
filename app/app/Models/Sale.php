e96f008a232f24421c4bd49d481ac8827fb16852
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-24 19:50:31 -0300

    formatter amount and add diff installment amount in first installment

--- app/app/Models/Sale.php
+++ app/app/Models/Sale.php
@@ -18,13 +18,26 @@ class Sale extends Model
         'totalInstallments'
     ];
 
+    public function setAmountAttribute($value)
+    {
+        $value = strval($value);
+        $value = substr_replace($value, '.', -2, 0);
+        $this->attributes['amount'] = number_format((float) $value, 2, '.', '');
+    }
+
     public function setTotalInstallmentsAttribute($value)
     {
         $installments = [];
-        $valueByInstallment = $this->amount / $value;
+        $amount = number_format((floor(($this->amount / $value) * 100) / 100), 2, '.', '');
+        $amountFirtsInstallment = number_format(($this->amount - ($amount * $value)), 2, '.', '');
         for ($i = 0; $i < $value; $i++) {
+            $amountInstallment = $amount;
+            if ($i == 0) {
+                $amountInstallment += $amountFirtsInstallment;
+            }
             $installment = new Installment([
-                'amount' => $valueByInstallment
+                'installment' => $i + 1,
+                'amount' => $amountInstallment
             ]);
             array_push($installments, $installment);
         }
