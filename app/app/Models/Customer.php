7a97bc9ec15e64d98fc23199ee107c93eb59f82f
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-24 17:21:08 -0300

    add customer model

---
+++ app/app/Models/Customer.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Customer extends Model
+{
+    protected $fillable = [
+        'name', 'address'
+    ];
+
+    public function setNameAttribute($value)
+    {
+        $this->attributes['name'] = trim($value);
+    }
+}
