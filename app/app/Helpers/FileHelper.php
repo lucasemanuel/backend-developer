213c22ab8ccf4477ea47eff5fb621252000392cb
Lucas Emanuel <lucaxemanuel@gmail.com>
2021-08-23 21:29:33 -0300

    ensure split string by sizes

--- app/app/Helpers/FileHelper.php
+++ app/app/Helpers/FileHelper.php
@@ -4,9 +4,27 @@ namespace App\Helpers;
 
 class FileHelper
 {
-    public static function splitByRow($file)
+    const SIZES_TO_SPLIT = [3, 8, 10, 2, 20, 8];
+
+    public static function splitByRow($file): array
     {
         $text = $file->get();
         return explode(PHP_EOL, $text);
     }
+
+    public static function splitBySizes($string, $sizesToSplit = self::SIZES_TO_SPLIT)
+    {
+        $terms = [];
+        $position = 0;
+
+        for ($i = 0; $i < count($sizesToSplit); $i++) {
+            $size = $sizesToSplit[$i];
+            $term = substr($string, $position, $size);
+            $position += $size;
+            array_push($terms, $term);
+        }
+
+        return $terms;
+    }
 }
+
